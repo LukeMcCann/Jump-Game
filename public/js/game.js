354975e11fff52f30d3d33ea47bbf1e948c16e7f
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-06 23:21:57 +0000

    Add basic experiment

--- public/js/game.js
+++ public/js/game.js
@@ -1,1 +1,25 @@
-alert('hello');
+const player = document.getElementById('player');
+const block = document.getElementById('block');
+document.addEventListener('click', jump);
+
+
+function jump() {
+    if (player.classList.contains('animate')) { return; }
+
+    player.classList.add('animate');
+    setTimeout(removeJump, 300);
+};
+
+function removeJump() {
+    player.classList.remove('animate');
+}
+
+function checkDead() {
+    let characterTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+        alert('Game over');
+    }
+}
+
+setInterval(checkDead, 10);
